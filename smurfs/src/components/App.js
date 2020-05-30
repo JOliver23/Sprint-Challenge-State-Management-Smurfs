0255b61dcccf91b8db7dbaa440ef7ac1dc9368ef
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-05-29 20:12:53 -0700

    cant get formstate posted to server data; unfamiliar w/ post

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -7,19 +7,20 @@ import { SmurfContext } from '../contexts/SmurfContext';
 
 
 function App() {
-  const smurfState = [{
+  const smurfs = [{
     name: "",
     age: "",
-    hieght: ""
+    hieght: "",
+    id: Date.now()
   }];
 
-  const [formState, setFormState] = useState(smurfState);
+  const [formState, setFormState] = useState(smurfs);
 
   useEffect(() => {
     axios
       .get("http://localhost:3333/smurfs")
       .then(res => {
-        smurfState.push(res.data)
+        console.log(res)
       })
       .catch(err => {
         console.log("err ", err)
@@ -28,21 +29,32 @@ function App() {
 
   const handleSubmit = e => {
     e.preventDefault();
-    axios.post("http://localhost:3333/smurfs", smurfState)
+    console.log("formState: ", formState)
+    axios.post("http://localhost:3333/smurfs", formState)
   }
+
+  const inputChange = e => {
+    e.persist(); 
+    const newFormData = {
+        ...formState,
+        [e.target.name]: e.target.value
+    }; 
+    setFormState(newFormData); // update state with new data
+  };
+
   return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
-        <form>
-          <input type='text' placeholder="Smurf Name" name="smurf-name" value={SmurfContext.name} />
-          <input type="text" placeholder="Smurf Age" name="smurf-age" value={SmurfContext.age} />
-          <input type='text' placeholder="Smurf Hieght" name='smurf-ht' value={SmurfContext.hieght} />
-          <button>Send To Village</button>
+        <form onSubmit={handleSubmit}>
+          <input type='text' placeholder="Smurf Name" name="name" onChange={inputChange} value={formState.name} />
+          <input type="text" placeholder="Smurf Age" name="age" onChange={inputChange} value={formState.age} />
+          <input type='text' placeholder="Smurf Hieght" name='hieght' onChange={inputChange} value={formState.hieght} />
+          <button type="submit">Send To Village</button>
         </form>
-        <SmurfContext.Provider value={{smurfState}}>
+        <SmurfContext.Provider value={{smurfs}}>
           <SmurfVillage />
         </SmurfContext.Provider>
       </div>
