3d79e4cec936552e90ba660a59f353de81759dc5
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-05-29 19:49:17 -0700

    lost

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,25 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SmurfVillage from './SmurfVillage';
 import axios from 'axios';
 import "./App.css";
 
-import {SmurfContext } from '../contexts/SmurfContext';
+import { SmurfContext } from '../contexts/SmurfContext';
 
 
 function App() {
-  const smurfs = [];
+  const smurfState = [{
+    name: "",
+    age: "",
+    hieght: ""
+  }];
+
+  const [formState, setFormState] = useState(smurfState);
 
   useEffect(() => {
     axios
       .get("http://localhost:3333/smurfs")
       .then(res => {
-        smurfs.push(res.data)
+        smurfState.push(res.data)
       })
       .catch(err => {
         console.log("err ", err)
       })
   })
-console.log(smurfs);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios.post("http://localhost:3333/smurfs", smurfState)
+  }
   return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
@@ -27,12 +37,12 @@ console.log(smurfs);
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
         <form>
-          <input type='text' placeholder="Smurf Name" name="smurf-name" />
-          <input type="text" placeholder="Smurf Age" name="smurf-age" />
-          <input type='text' placeholder="Smurf Hieght" name='smurf-ht' />
+          <input type='text' placeholder="Smurf Name" name="smurf-name" value={SmurfContext.name} />
+          <input type="text" placeholder="Smurf Age" name="smurf-age" value={SmurfContext.age} />
+          <input type='text' placeholder="Smurf Hieght" name='smurf-ht' value={SmurfContext.hieght} />
           <button>Send To Village</button>
         </form>
-        <SmurfContext.Provider value={{smurfs}}>
+        <SmurfContext.Provider value={{smurfState}}>
           <SmurfVillage />
         </SmurfContext.Provider>
       </div>
